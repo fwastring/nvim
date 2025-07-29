c4b527deea23119e4cf3559542dd6a1ca399df25
fwastring <fredrik@wastring.com>
2025-07-29 09:08:03 +0200

    Fixed some packages

--- init.lua
+++ init.lua
@@ -1,40 +1,27 @@
 vim.pack.add({
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",        version = 'main' },
+	{ src = 'https://github.com/akinsho/bufferline.nvim', version = "v4.9.1" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",  version = 'main' },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/qvalentin/helm-ls.nvim", ft = "helm" },
-	{
-		src = "https://github.com/catppuccin/nvim",
-	},
+	{ src = "https://github.com/kdheepak/lazygit.nvim" },
+	{ src = "https://github.com/catppuccin/nvim" },
+	{ src = 'https://github.com/rmagatti/auto-session' },
+	{ src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
+	{ src = 'https://github.com/hrsh7th/nvim-cmp' },
+	{ src = "https://github.com/gbprod/substitute.nvim" },
+	{ src = 'https://github.com/numToStr/Comment.nvim' },
 	{
 		src = 'https://github.com/windwp/nvim-autopairs',
 		event = "InsertEnter",
 		config = true
 	},
-	{
-		src = 'https://github.com/rmagatti/auto-session',
-	},
-	{
-		src = 'https://github.com/hrsh7th/cmp-nvim-lsp'
-	},
-	{
-		src = 'https://github.com/hrsh7th/nvim-cmp'
-	},
-	{
-		src = "https://github.com/gbprod/substitute.nvim",
-		opts = {}
-	},
-	{
-		src = 'https://github.com/numToStr/Comment.nvim',
-		opts = {},
-		lazy = false,
-	},
 })
 
 vim.lsp.enable({
-	'vue_ls',
+	'vtsls',
 	'omnisharp',
 	'gopls',
 	'nixd',
@@ -54,10 +41,40 @@ vim.lsp.enable({
 	'helm_ls',
 })
 
+vim.lsp.config("vtsls", {
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+				settings = {
+					vtsls = { tsserver = { globalPlugins = {} } },
+					typescript = {
+						inlayHints = {
+							parameterNames = { enabled = "literals" },
+							parameterTypes = { enabled = true },
+							variableTypes = { enabled = true },
+							propertyDeclarationTypes = { enabled = true },
+							functionLikeReturnTypes = { enabled = true },
+							enumMemberValues = { enabled = true },
+						},
+					},
+				},
+				before_init = function(_, config)
+					table.insert(config.settings.vtsls.tsserver.globalPlugins, {
+						name = "@vue/typescript-plugin",
+						location = "/run/current-system/sw/bin/vue-language-server",
+						languages = { "vue" },
+						configNamespace = "typescript",
+						enableForWorkspaceTypeScriptVersions = true,
+					})
+				end,
+				on_attach = function(client)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
+			})
+
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
-vim.o.termguicolors = true
 vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.g.mapleader = " "
@@ -70,43 +87,64 @@ vim.o.cmdheight = 0
 
 
 -- Configuration of plugins
+require("catppuccin").setup({
+    default_integrations = false,
+})
+local bufferline = require("bufferline")
+local mocha = require("catppuccin.palettes").get_palette "latte"
+bufferline.setup {
+    highlights = require("catppuccin.groups.integrations.bufferline").get {
+        styles = { "italic", "bold" },
+        custom = {
+            all = {
+                fill = { bg = "#000000" },
+            },
+            mocha = {
+                background = { fg = mocha.text },
+            },
+            latte = {
+                background = { fg = "#000000" },
+            },
+        },
+    },
+}
 require("auto-session").setup {
 	log_level = "error",
-	auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+	auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
 }
 require 'nvim-treesitter'.setup()
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 
 parser_config.csharp = {
-  install_info = {
-    url = "https://github.com/tree-sitter/tree-sitter-c-sharp", -- local path or git repo
-    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
-    branch = "master", -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-  },
-  filetype = "cs", -- if filetype does not match the parser name
+	install_info = {
+		url = "https://github.com/tree-sitter/tree-sitter-c-sharp", -- local path or git repo
+		files = { "src/parser.c" },                             -- note that some parsers also require src/scanner.c or src/scanner.cc
+		branch = "master",                                      -- default branch in case of git repo if different from master
+		generate_requires_npm = false,                          -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = false,                 -- if folder contains pre-generated src/parser.c
+	},
+	filetype = "cs",                                            -- if filetype does not match the parser name
 }
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "css", "c", "lua", "vim", "vimdoc", "query", "python", "bash",  "nix", "dockerfile", "csharp", "markdown", "json", "typescript" },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = { "vue" },
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = { "helm",  "css", "c", "lua", "vim", "vimdoc", "query", "python", "bash", "nix", "dockerfile", "csharp", "markdown", "json", "typescript" },
+	sync_install = false,
+	auto_install = true,
+	ignore_install = { "vue" },
 
-  highlight = {
-    enable = true,
+	highlight = {
+		enable = true,
 
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-    additional_vim_regex_highlighting = false,
-  },
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+		additional_vim_regex_highlighting = false,
+	},
 }
 local cmp = require('cmp')
 
@@ -142,17 +180,26 @@ cmp.setup({
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
+	'confirm_done',
+	cmp_autopairs.on_confirm_done()
 )
 
 require "mini.pick".setup()
+
+require('substitute').setup()
+
+-- Sub
+vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
+vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
+vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
+vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
 
 
 -- LSP
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', 'go', vim.diagnostic.open_float)
 vim.keymap.set('n', 'gd', "<C-]><CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", { noremap = true, silent = true })
 
 -- Good navigation mappings for wrap
 vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
